2d43a01d61a61f16b1fb5fc2797d72002e16633e
Yousuf Khan Ratul <ratulcse27@gmail.com>
2015-09-15 01:27:29 +0600

    final

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var app     = express();
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 
 app.use(cors());
 
@@ -22,10 +22,9 @@ app.get('/', function(req, res){
     request(url, function(error, response, html){
         if(!error){
 
-            res.json(response.statusCode);
 
 
-            /*var jsonObj = JSON.parse(html);
+            var jsonObj = JSON.parse(html);
             var content = jsonObj.response.content.replace(/[-a-z0-9:\.]/ig,'');
             var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
             var spaceRE = /\s+/g;
@@ -34,8 +33,8 @@ app.get('/', function(req, res){
 
             console.log(content.length);
 
-            res.json(s.words($(content).strip( '_', '-','>','\n').s));
-            //res.json($(content).lines());*/
+            //res.json(s.words($(content).strip( '_', '-','>','\n').s));
+            res.json($(content).lines());
         }
     });
 });
@@ -88,7 +87,100 @@ app.post('/content',function(req,res){
     });
 
     //res.json(req.body.name);
-})
+});
+
+app.post('/lines',function(req,res){
+
+    var url = req.body.name;
+
+    var fullURL =  "http://boilerpipe-web.appspot.com/extract?url="+url+"&extractor=ArticleExtractor&output=json&extractImages=";
+
+    request(fullURL, function(error, response, html){
+        if(!error){
+
+            //console.log(response);
+            if(response.headers['content-type'] === "text/html; charset=utf-8"){
+                res.status(404).json({ error: 'error' });
+                console.log('html');
+
+            }
+            else if(response.statusCode !== 200){
+                res.status(404).json({ error: 'message' });
+                console.log('200');
+            }
+            else{
+                if(response.headers['content-type'] === "application/json; charset=UTF-8"){
+                    var jsonObj = JSON.parse(html);
+                    var content = jsonObj.response.content.replace(/[-a-z0-9:\.]/ig,'');
+                    var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+                    var spaceRE = /\s+/g;
+                    content.replace(punctRE, '').replace(spaceRE, ' ');
+
+                    var lines = $(content).lines()
+                    //console.log(wordSet);
+
+                    res.status(200).json({'lines' : lines});
+                }
+
+            }
+
+
+        }else{
+            res.status(404).json({ error: 'message' });
+        }
+    });
+
+    //res.json(req.body.name);
+});
+
+app.post('/words',function(req,res){
+
+    var url = req.body.name;
+
+    var fullURL =  "http://boilerpipe-web.appspot.com/extract?url="+url+"&extractor=ArticleExtractor&output=json&extractImages=";
+
+    request(fullURL, function(error, response, html){
+        if(!error){
+
+            //console.log(response);
+            if(response.headers['content-type'] === "text/html; charset=utf-8"){
+                res.status(404).json({ error: 'error' });
+                console.log('html');
+
+            }
+            else if(response.statusCode !== 200){
+                res.status(404).json({ error: 'message' });
+                console.log('200');
+            }
+            else{
+                if(response.headers['content-type'] === "application/json; charset=UTF-8"){
+                    var jsonObj = JSON.parse(html);
+                    var content = jsonObj.response.content;
+
+                    var words = content.replace(/[-a-z0-9:\.]/ig,'');
+                    var punctRE = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~|]/g;
+                    var spaceRE = /\s+/g;
+                    words.replace(punctRE, '').replace(spaceRE, ' ');
+
+
+                    //console.log(content.length);
+
+                    var wordSet = s.words($(words).strip( '_', '-','>','\n').s);
+                    //console.log(wordSet);
+
+                    res.status(200).json({'wordSet' : wordSet});
+                }
+
+            }
+
+
+        }else{
+            res.status(404).json({ error: 'message' });
+        }
+    });
+
+    //res.json(req.body.name);
+});
 
 
 
